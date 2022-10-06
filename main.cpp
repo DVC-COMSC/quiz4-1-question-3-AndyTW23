d0996fd95e9803bedad2819065b8813b4f879db4
AndyTW23 <112031703+AndyTW23@users.noreply.github.com>
2022-10-06 00:52:47 +0000

    Code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,64 @@
 // Complete the following program. 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	const double rate1 = 1.10;
-	const double rate2 = 2.20;
-	const double rate3 = 3.70;
-	const double rate4 = 4.80;
-	double total_charge;
-	double weight, distance, rate;
 
 	
         // **********************
         // Your code 
         // **********************
         
-	cout << total_charge << endl;
+	// Make a program that finds the minimum and maximum values among three integer values.
+
+// Make a program that finds the minimum and maximum values among three integer values.
+
+// int num1, num2, num3;
+// cin >> num1 >> num2 >> num3;
+// Find the min and max among three values.
+// Display the numbers with ascending order ( min, other, max).
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int num1, num2, num3;
+  int min, max, other;
+  
+  cin >> num1 >> num2 >> num3;
+
+  if (num1 > num2)
+  {
+    if ( num2 < num3)
+      min = num2;
+        else //num2 >= num3
+     min = num3;
+    other = num1;
+    }
+  else // num1 <= num2
+  {
+    if (num1 < num3)
+     min = num1;
+    else 
+     min = num3;
+    other = num2;
+  }//max numbers
+    if (num1 < num2)
+  {
+    if ( num2 > num3)
+     max = num2;
+        else //num2 >= num3
+       max = num3;
+      other = num1;
+    }
+  else // num1 >= num2
+  {
+    if (num1 > num3)
+  max = num1;
+    else 
+    max = num3;
+    other = num2;
+  }
+  cout << "max:" << max << endl;
+  cout << "other:" << other << endl;
+  cout << "min:" << min << endl;
+  //other value
 }
